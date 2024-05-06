d5ec3e0fc716743a2e14607355aa306f8e0d0e1e
Anujkulal <anujkulal333@gmail.com>
2024-05-06 21:29:43 +0530

    file added commits

---
+++ 1_acsiiValue.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main() {
+    char c;
+    printf("Enter the character: ");
+    scanf("%c", &c);
+    printf("Ascii value of %c is %d\n",c,c);
+    return 0;
+}
