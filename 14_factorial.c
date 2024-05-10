3b259c8772abf2a6a17490041d66c28cad6d0b3f
Anujkulal <anujkulal333@gmail.com>
2024-05-10 20:27:07 +0530

    initial commit

--- 14_factorial.c
+++ 14_factorial.c
@@ -1,15 +1,54 @@
-//to find the factorial of a number
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
-int main() {
+// //to find the factorial of a number
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+int main()
+{
     int n, i, fact = 1;
     printf("Enter the number: ");
     scanf("%d", &n);
-    for(i=1; i<=n; i++){
-        fact *= i;
+
+    if (n < 0)
+        printf("Error!, factorial of a number cannot be negative\n");
+    else
+    {
+        for (i = 1; i <= n; i++)
+            fact *= i;
+        printf("The factorial of a number is %d\n", fact);
     }
-    printf("The factorial of a number is %d\n", fact);
+
     return 0;
 }
+
+// // // using recursive function
+//
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <math.h>
+// #include <string.h>
+//  double factorial(double n);
+// int main()
+// {
+//     double n, i, fact = 1;
+//     printf("Enter the number: ");
+//     scanf("%lf", &n);
+//     int fact1 = factorial(n);
+//     if (fact1 == -1)
+//         printf("Error!, factorial of a number cannot be negative\n");
+//     else
+//         printf("The factorial of a number is %lf\n", factorial(n));
+//     return 0;
+// }
+
+// double factorial(double n)
+// {
+//     if (n < 0)
+//         return -1;
+//     else if (n == 0 || n == 1)
+//         return 1;
+//     else
+//     {
+//         return n * factorial(n - 1);
+//     }
+// }
