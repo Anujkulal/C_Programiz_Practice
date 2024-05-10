d5e28de6e1794d7f835b71a7d25e0645783d2723
Anujkulal <anujkulal333@gmail.com>
2024-05-10 19:40:14 +0530

    initial commit

--- 10_leapYear.c
+++ 10_leapYear.c
@@ -3,13 +3,14 @@
 #include<stdlib.h>
 #include<math.h>
 #include<string.h>
+
 int main() {
     int year;
     printf("Enter the year: ");
     scanf("%d", &year);
-    if(year % 400 ==0)
+    if(year % 400 ==0) // if a num divisible by 400 then it is a leap year
         printf("%d is a leap year\n",year);
-    else if(year % 100 == 0)
+    else if(year % 100 == 0) //
         printf("%d is not a leap year\n",year);
     else if(year % 4 == 0)
         printf("%d is a leap year\n", year);
