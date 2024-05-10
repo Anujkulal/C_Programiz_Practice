19782588bebc39956ee8136a57ab624607a25b6f
Anujkulal <anujkulal333@gmail.com>
2024-05-10 19:55:48 +0530

    initial commit

---
+++ 13_naturalNumSum.c
@@ -0,0 +1,15 @@
+//sum of natural numbers
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+int main() {
+    // // For loop
+    int n, sum=0;
+    printf("Enter the number of natural numbers: ");
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++)
+        sum+=i;
+    printf("The sum of natural numbers is %d\n",sum);
+    return 0;
+}
