1650c9e8703ee11df5c488cfaf2c09b95b4d00ee
Anujkulal <anujkulal333@gmail.com>
2024-05-16 18:21:23 +0530

    initial commit

--- 16_fibonacci.c
+++ 16_fibonacci.c
@@ -4,6 +4,16 @@
 #include<math.h>
 #include<string.h>
 int main() {
-    
+    int a = 0, b = 1, next, n, i;
+    printf("Enter the no of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci series: %d %d ",a, b);
+    next = a + b;
+    for(i=2; i<n; i++){
+        printf("%d ",next);
+        a = b;
+        b = next;
+        next = a + b;
+    }
     return 0;
 }
