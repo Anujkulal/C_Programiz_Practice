e6b566ba79cfecdb81227771cfae9122e8cb71ed
Anujkulal <anujkulal333@gmail.com>
2024-05-18 21:06:22 +0530

    initial commit

---
+++ 19_alphabetPrint.c
@@ -0,0 +1,17 @@
+//to display alphabets
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+int main() {
+    // for(int i = 65; i<=90; i++){
+    //     printf("%c ",i);
+    // }printf("\n");
+
+    // // or 
+
+    for(char i = 'A'; i<='Z'; i++)
+    printf("%c ",i);
+    printf("\n");
+    return 0;
+}
