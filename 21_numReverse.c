4b8e41523824bd647f18b15d5b30db3865315382
Anujkulal <anujkulal333@gmail.com>
2024-05-19 21:13:25 +0530

    23rd commit

--- 21_numReverse.c
+++ 21_numReverse.c
@@ -7,11 +7,12 @@ int main() {
     int num, i, reverse = 0, remainder;
     printf("Enter the digit: ");
     scanf("%d", &num);
+    int original = num;
     while(num != 0){
         remainder = num % 10;
         reverse = reverse * 10 + remainder;
         num = num / 10;
     }
-    printf("Reversed number: %d\n", reverse);
+    printf("Original num: %d\nReversed number: %d\n",original, reverse);
     return 0;
 }
