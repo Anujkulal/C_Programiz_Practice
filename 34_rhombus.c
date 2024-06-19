000a34b9835a790dcc9ebbfd66b66df9841d5279
anujkulal <anujkulal333@gmail.com>
2024-06-19 07:13:50 +0530

    commit

---
+++ 34_rhombus.c
@@ -0,0 +1,20 @@
+//Rhombus pattern
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+int main() {
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<i; j++){
+            printf(" "); //removed 1 extra space from inverted left half pyramid code which gives inverted full pyramid
+        }
+        for(int k=1; k<=n; k++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
